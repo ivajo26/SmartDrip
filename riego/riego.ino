4a3cdc968f893f2231cc1bc162b27f173e417b3d
ivajo26 <idiaz@devhunter.co>
2015-12-02 09:11:12 -0500

    No usar funcion delay

--- riego/riego.ino
+++ riego/riego.ino
@@ -4,6 +4,9 @@
 Humedad sensorH = Humedad(A0,A1,A2,A3);
 int relay = 12;
 Temperature sensorT = Temperature(A4);
+bool valvula_estado = false;
+int tiempo_de_bombeo = 3000;
+int tiempo_de_cierre = 0 ;
 
 void setup(){
   Serial.begin(9600);
@@ -24,9 +27,14 @@ void loop(){
   Serial.print(sensorT.getTemperature());
   Serial.print(":");
   if (sensorH.getPromedio() <= 300){
-    digitalWrite(relay,HIGH);
-    delay(3000);
-    digitalWrite(relay,LOW);
+    if(valvula_estado!=true){
+      digitalWrite(relay,HIGH);
+      tiempo_de_cierre = millis()+tiempo_de_bombeo;
+      valvula_estado=true;
+    }
    }
+  if(tiempo_de_cierre<=millis()){
+    digitalWrite(relay,LOW);
+    valvula_estado=false;
+  }
 }
-
